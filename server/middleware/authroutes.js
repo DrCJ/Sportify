8404296e2a9c09f0f9b2b3355fc84acb9eb14223
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-08 16:47:00 -0700

    Convert yahoo queries to promises

--- server/middleware/authroutes.js
+++ server/middleware/authroutes.js
@@ -1,16 +1,29 @@
 module.exports = (app, express, passport) => {
+  const getTeam = (yahoo) => {
+    return new Promise((resolve, reject) => {
+      yahoo.user.game_teams('359', (err, data) => {
+        if (err) { reject(err); }
+        const teamKey = data.teams[0].teams[0].team_key.toString();
+        const teamObj = { teamKey, yahoo };
+        resolve(teamObj);
+      });
+    });
+  };
+
+  const getPlayers = (teamObj) => {
+    return new Promise((resolve, reject) => {
+      teamObj.yahoo.team.roster(teamObj.teamKey, (err, data) => {
+        if (err) { reject(err); }
+        resolve(data.roster);
+      });
+    });
+  };
+
   const getRoster = (req, res) => {
     const yahoo = req.app.yf;
-    if (yahoo) {
-      yahoo.user.game_teams('359', (err, data) => {
-        if (data) {
-          const teamKey = data.teams[0].teams[0].team_key.toString();
-          yahoo.team.roster(teamKey, (e, subdata) => {
-            res.json(subdata.roster);
-          });
-        }
-      });
-    }
+    getTeam(yahoo)
+      .then(getPlayers)
+      .then((roster) => res.json(roster));
   };
 
   app.use('/', express.static(`${__dirname}/../../public`));
@@ -31,8 +44,8 @@ module.exports = (app, express, passport) => {
   app.get('/roster', getRoster);
 
   app.get('/logout', (req, res) => {
-    req.app.yf = null;
     req.session.destroy(() => {
+      req.app.yf = null;
       res.redirect('/');
     });
   });
