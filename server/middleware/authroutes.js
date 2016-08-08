8909c50ce6702549b78fd69d07550087bb548b67
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-08 15:16:43 -0700

    Abstract /roster endpoints request handler to its own function

--- server/middleware/authroutes.js
+++ server/middleware/authroutes.js
@@ -1,4 +1,18 @@
 module.exports = (app, express, passport) => {
+  const getRoster = (req, res) => {
+    const yahoo = req.app.yf;
+    if (yahoo) {
+      yahoo.user.game_teams('359', (err, data) => {
+        if (data) {
+          const teamKey = data.teams[0].teams[0].team_key.toString();
+          yahoo.team.roster(teamKey, (e, subdata) => {
+            res.json(subdata.roster);
+          });
+        }
+      });
+    }
+  };
+
   app.use('/', express.static(`${__dirname}/../../public`));
 
   app.get('/auth/yahoo',
@@ -14,19 +28,7 @@ module.exports = (app, express, passport) => {
     }
   );
 
-  app.get('/roster', (req, res) => {
-    const yahoo = req.app.yf;
-    if (yahoo) {
-      yahoo.user.game_teams('359', (err, data) => {
-        if (data) {
-          const teamKey = data.teams[0].teams[0].team_key.toString();
-          yahoo.team.roster(teamKey, (e, subdata) => {
-            res.json(subdata.roster);
-          });
-        }
-      });
-    }
-  });
+  app.get('/roster', getRoster);
 
   app.get('/logout', (req, res) => {
     req.app.yf = null;
