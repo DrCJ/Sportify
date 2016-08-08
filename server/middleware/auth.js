0fc0de2fb5a85a90f754ad19219cd85733bbe5dc
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-08 11:28:03 -0700

    Abstract into separate files

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,36 +3,34 @@ const YahooStrategy = require('https-passport-yahoo-oauth').Strategy;
 const YahooFantasy = require('yahoo-fantasy');
 const { consumerKey, consumerSecret, callbackURL } = require('./../config/credentials');
 
-const yf = new YahooFantasy(consumerKey, consumerSecret);
+module.exports = (app) => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+  passport.deserializeUser((obj, done) => {
+    done(null, obj);
+  });
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+  passport.use(new YahooStrategy({ consumerKey, consumerSecret, callbackURL },
+    (accessToken, tokenSecret, profile, done) => {
+      const data = profile._json;
+      const userObj = {
+        id: profile.id,
+        name: data.profile.nickname,
+        avatar: data.profile.image.imageUrl,
+        dateJoined: new Date().getTime(),
+        lastUpdated: new Date().getTime(),
+        lastVisit: new Date().getTime(),
+        accessToken,
+        tokenSecret,
+        sessionHandle: profile.oauth_session_handle,
+      };
+      app.yf = new YahooFantasy(consumerKey, consumerSecret);
+      app.yf.setUserToken(userObj.accessToken, userObj.tokenSecret, userObj.sessionHandle);
+      return done(null, userObj);
+    })
+  );
 
-passport.use(new YahooStrategy({ consumerKey, consumerSecret, callbackURL },
-  (accessToken, tokenSecret, profile, done) => {
-    const data = profile._json;
-
-    const userObj = {
-      id: profile.id,
-      name: data.profile.nickname,
-      avatar: data.profile.image.imageUrl,
-      dateJoined: new Date().getTime(),
-      lastUpdated: new Date().getTime(),
-      lastVisit: new Date().getTime(),
-      accessToken,
-      tokenSecret,
-      sessionHandle: profile.oauth_session_handle,
-    };
-
-    yf.setUserToken(userObj.accessToken, userObj.tokenSecret, userObj.sessionHandle);
-
-    return done(null, userObj);
-  })
-);
-
-module.exports = { passport, yf };
+  return passport;
+};
--- server/server.js
+++ server/server.js
@@ -1,47 +1,13 @@
 // const db = require('./db/db_config');
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const { passport, yf } = require('./middleware/auth');
+const middleware = require('./middleware/middleware');
+const auth = require('./middleware/auth');
+const routes = require('./middleware/authroutes');
 
 const app = express();
-
-app.use(bodyParser());
-app.use(cookieParser());
-app.use(session({ secret: 'djrc' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.yf = yf;
-
-app.use('/', express.static(`${__dirname}/../public`));
-
-app.get('/auth/yahoo',
-  passport.authenticate('yahoo', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect('/');
-  });
-
-app.get('/auth/yahoo/callback',
-  passport.authenticate('yahoo', { failureRedirect: '/login' }),
-  (req, res) => {
-    // res.redirect('req.session.redirect || '/')';
-    res.redirect('/');
-  }
-);
-
-app.get('/roster', (req, res) => {
-  const yahoo = req.app.yf;
-
-  yahoo.user.game_teams('359', (err, data) => {
-    if (data) {
-      const teamKey = data.teams[0].teams[0].team_key.toString();
-      yahoo.team.roster(teamKey, (e, subdata) => {
-        res.json(subdata.roster);
-      });
-    }
-  });
-});
+const passport = auth(app, express);
+middleware(app);
+routes(app, express, passport);
 
 app.listen(80, () => {
   console.log('currently listening to port 80');
