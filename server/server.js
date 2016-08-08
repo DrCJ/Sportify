15e0ed31edad9dd0b3d94e9b6c96765b1baba99f
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-07 18:08:58 -0700

    Delete old AWS server code, add passport definitions

--- server/server.js
+++ server/server.js
@@ -1,49 +1,48 @@
 const db = require('./db/db_config');
 const express = require('express');
+const passport = require('passport');
+const YahooStrategy = require('https-passport-yahoo-oauth').Strategy
+const { consumerKey, consumerSecret, callbackURL } = require('./config/credentials');
+
 const app = express();
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(
+  new YahooStrategy({
+    consumerKey,
+    consumerSecret,
+    callbackURL,
+  },
+  (accessToken, tokenSecret, profile, done) => {
+    const data = profile._json;
+
+    const userObj = {
+      id: profile.id,
+      name: data.profile.nickname,
+      avatar: data.profile.image.imageUrl,
+      dateJoined: new Date().getTime(),
+      lastUpdated: new Date().getTime(),
+      lastVisit: new Date().getTime(),
+      accessToken,
+      tokenSecret,
+      sessionHandle: profile.oauth_session_handle,
+    };
+
+    app.yf.setUserToken(userObj.accessToken, userObj.tokenSecret, userObj.sessionHandle);
+
+    return done(null, userObj);
+  })
+);
 
 app.use('/', express.static(`${__dirname}/../public`));
 
 app.listen(80, () => {
   console.log('currently listening to port 3000');
 });
-
-// let port = process.env.PORT || 3000,
-//     http = require('http'),
-//     fs = require('fs'),
-//     html = fs.readFileSync(__dirname + '/../public/index.html');
-//
-// const log = function(entry) {
-//     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-// };
-//
-// const server = http.createServer((req, res) => {
-//     if (req.method === 'POST') {
-//         const body = '';
-//
-//         req.on('data', function(chunk) {
-//             body += chunk;
-//         });
-//
-//         req.on('end', function() {
-//             if (req.url === '/') {
-//                 log('Received message: ' + body);
-//             } else if (req.url = '/scheduled') {
-//                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-//             }
-//
-//             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-//             res.end();
-//         });
-//     } else {
-//         res.writeHead(200);
-//         res.write(html);
-//         res.end();
-//     }
-// });
-//
-// // Listen on port 3000, IP defaults to 127.0.0.1
-// server.listen(port);
-//
-// // Put a friendly message on the terminal
-// console.log('Server running at http://127.0.0.1:' + port + '/');
