5b53a12cb0be1416de38ae5ea9b7c5e9776d9624
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-10 12:02:42 -0700

    Adds table with players from yahoo team

--- client/src/containers/YahooTeams.js
+++ client/src/containers/YahooTeams.js
@@ -14,25 +14,71 @@ class YahooTeams extends Component {
   renderRoster() {
     return this.props.tempTableData.map((player, index) => {
       return (
-        <li key={index}>
-          <Link to={`posts/${index}`}>
-            <span>{player.name.first} {player.name.last}</span>
-          </Link>
-        </li>
+        <tbody key={index}>
+          <tr>
+            <td>
+              <Link to={`posts/${index}`}>
+                <span>{player.name.first} {player.name.last}</span>
+              </Link>
+            </td>
+            <td>22</td>
+            <td>33</td>
+            <td>44</td>
+            <td>55</td>
+            <td>66</td>
+            <td>77</td>
+            <td>554</td>
+            <td>5666</td>
+            <td>3333</td>
+            <td>545</td>
+            <td>5454</td>
+            <td>3</td>
+            <td>5</td>
+            <td>23</td>
+            <td>55</td>
+            <td>67</td>
+            <td>4232</td>
+          </tr>
+        </tbody>
       );
     });
   }
 
   render() {
     return (
-      <div>
+      <div className="center-content">
         <a href="/auth/yahoo"> Login </a>
         <a href="/logout">Logout</a>
         <YahooProfileImage />
-        <ul>
-          {this.renderRoster()}
-        </ul>
-      </div>
+        <div>general info</div>
+          <div className="none">
+            <table>
+              <thead>
+                <tr>
+                  <td>Offense</td>
+                  <td>Owner</td>
+                  <td>GP*</td>
+                  <td>% Owned</td>
+                  <td>Proj</td>
+                  <td>Actual</td>
+                  <td>Yds</td>
+                  <td>TD</td>
+                  <td>Int</td>
+                  <td>Att*</td>
+                  <td>Yds</td>
+                  <td>TD</td>
+                  <td>Tgt*</td>
+                  <td>Rec</td>
+                  <td>TD</td>
+                  <td>TD</td>
+                  <td>2PT</td>
+                  <td>Lost</td>
+                </tr>
+              </thead>
+              {this.renderRoster()}
+            </table>
+          </div>
+        </div>
     );
   }
 }
