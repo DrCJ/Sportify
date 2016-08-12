a13fe649f6c43216fb24d7641fe8e3d72aec53ac
Roy Eun <rooyeun@gmail.com>
2016-08-11 19:34:00 -0700

    Modularize the Playerview Component. PlayerListTable and PlayerListForm created

--- client/src/containers/PlayerView.js
+++ client/src/containers/PlayerView.js
@@ -2,34 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-import PlayerListView from './PlayerListView';
-import { reduxForm } from 'redux-form';
-import { requestAllPlayers, filterPlayers } from '../actions/index';
+import { PlayerViewTable } from '../component/PlayerViewTable';
+import { requestAllPlayers } from '../actions/index';
+import PlayerFilterForm from './PlayerFilterForm';
 
 class PlayerView extends Component {
   componentWillMount() {
     this.props.requestAllPlayers().then((data) => {
-      console.log(data, 'data');
       this.props.players.concat(data);
     });
   }
-
-  onSubmit(props) {
-    const reqObj = {
-      Team: this.props.fields.team.value,
-      Week: this.props.fields.weekly.value,
-      Position: this.props.fields.position.value,
-    };
-    console.log(reqObj);
-    this.props.filterPlayers(reqObj)
-      .then((data) => {
-        console.log(data);
-        // this.props.players.concat()
-      });
-    // Post Get All Players
-  }
   render() {
-    const { fields: { team, position, weekly }, handleSubmit } = this.props;
     if (this.props.players.length === 0) {
       return <div> Loading (Image????) </div>
     }
@@ -40,82 +23,8 @@ class PlayerView extends Component {
           <div className="search-container">
             <input type="text" name="name" value="" placeholder="SEARCH" />
           </div>
-          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-            <div className="filter-form-select">
-              <label for="teamSelect"> TEAM </label>
-              <select data="teamVal" id="teamSelect" {...team}>
-                <option value="All">All</option>
-                <option value="GB">GreenBay</option>
-              </select>
-            </div>
-            <div className="filter-form-select">
-              <label for="weekly"> WEEKLY </label>
-                <select data="weeklyVal" id="weeklySelect" {...weekly}>
-                  <option value="">2015-2016</option>
-                  <option value="1">Week 1</option>
-                  <option value="2">Week 2</option>
-                  <option value="3">Week 3</option>
-                  <option value="4">Week 4</option>
-                  <option value="5">Week 5</option>
-                  <option value="6">Week 6</option>
-                  <option value="7">Week 7</option>
-                  <option value="8">Week 8</option>
-                  <option value="9">Week 9</option>
-                  <option value="10">Week 10</option>
-                  <option value="11">Week 11</option>
-                  <option value="12">Week 12</option>
-                  <option value="13">Week 13</option>
-                  <option value="14">Week 14</option>
-                  <option value="15">Week 15</option>
-                  <option value="16">Week 16</option>
-                  <option value="17">Week 17</option>
-                </select>
-            </div>
-            <div className="filter-form-select">
-              <label for="positionSelect"> POSITION </label>
-                <select data="positionVal" id="positionSelect" {...position}>
-                  <option value="All">All</option>
-                  <option value="QB">QB</option>
-                  <option value="RB">RB</option>
-                  <option value="WR">WR</option>
-                  <option value="TE">TE</option>
-                  <option value="K">K</option>
-                  <option value="Def">Def</option>
-                </select>
-            </div>
-            <input
-              type="submit"
-              className="button filter-form-select-button"
-              value="Submit"
-            ></input>
-          </form>
-          <div className="player-table">
-            <table>
-              <thead>
-                <tr>
-                  <td>Offense</td>
-                  <td>Position</td>
-                  <td>GP*</td>
-                  <td>Opp</td>
-                  <td>Proj</td>
-                  <td>Actual</td>
-                  <td>Yds</td>
-                  <td>TD</td>
-                  <td>Int</td>
-                  <td>Att*</td>
-                  <td>Yds</td>
-                  <td>TD</td>
-                  <td>Tgt*</td>
-                  <td>Rec</td>
-                  <td>RshTD</td>
-                  <td>RecTD</td>
-                  <td>2PT</td>
-                  <td>Carlos </td>
-                </tr>
-              </thead>
-            <PlayerListView />
-            </table>
-          </div>
+          <PlayerFilterForm />
+          <PlayerViewTable />
         </div>
       </div>
     );
@@ -128,7 +37,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default reduxForm({
-  form: 'PlayerView',
-  fields: ['team', 'position', 'weekly'],
-}, mapStateToProps, { requestAllPlayers, filterPlayers })(PlayerView);
+export default connect(mapStateToProps, { requestAllPlayers })(PlayerView);
