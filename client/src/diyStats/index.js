ec2abff10b8ddd95670e8154cd104099335100ec
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-17 17:29:41 -0700

    Add search functionality and player image

--- client/src/diyStats/index.js
+++ client/src/diyStats/index.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import teams from '../helpers/teamNames';
+import { fetchSpecificPlayers } from '../compare/actions';
 
 class DIYStatsView extends Component {
+  constructor(props) {
+    super(props);
+    this.onSearch = this.onSearch.bind(this);
+  }
   onCheck(event) {
     // console.log('checked!', event.target.name, event.target.checked);
   }
-  onSubmit(event) {
+  onFieldSubmit(event) {
     event.preventDefault();
+    console.log(event.target[0].value);
+  }
+  onSearch(event) {
+    event.preventDefault();
+    console.log(event.target[0].value);
+    this.props.fetchSpecificPlayers({ playerNames: [event.target[0].value] });
   }
   render() {
     const teamOptions = [];
+    let playerImage;
     for (let k in teams) {
       teamOptions.push(<option value={k}>{teams[k]}</option>);
+    }
+    if (this.props.players[0].player) {
+      playerImage = <img
+        className="player2-image"
+        src={this.props.players[0].player.image_url.substring(155)}
+      />;
     }
     return (
       <div className="center-content">
         <h1>DIY Stats View</h1>
         <h3>Choose a player:</h3>
         <div className="search-container">
-          <form>
+          <form onSubmit={this.onSearch}>
             <input type="text" name="name" placeholder="SEARCH" />
+            <button type="Submit" >Submit</button>
           </form>
         </div>
         <h3>Current Player:</h3>
+        {playerImage}
         <h3>Performance</h3>
         <input type="checkbox" name="0" /> Against a Team
         <input type="checkbox" name="1" /> On a Day of the Week
@@ -30,21 +51,25 @@ class DIYStatsView extends Component {
         <input type="checkbox" name="3" /> At Home/Away
         <input type="checkbox" name="4" /> Under Specific Weather Conditions
         <input type="checkbox" name="5" /> Started/Benched
-        <form onChange={this.onCheck}>
+        <form onSubmit={this.onFieldSubmit}>
           <div className="filter-form-select">
             <label htmlFor="teamSelect"> AGAINST A TEAM </label>
             <select data="teamVal" id="teamSelect">
-              <option value="">All</option>
               {teamOptions}
             </select>
           </div>
+          <button type="Submit" >Submit</button>
         </form>
       </div>
     );
   }
 }
 
-export default DIYStatsView;
+function mapStateToProps(state) {
+  return { players: state.players };
+}
+
+export default connect(mapStateToProps, { fetchSpecificPlayers })(DIYStatsView);
 
 // Player
   // Against Team
