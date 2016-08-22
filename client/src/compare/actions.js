b39143537fefad5feeba6ddd5a81c917835dd166
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-08-22 14:56:24 -0700

    Remove action types from compare folder

--- client/src/compare/actions.js
+++ client/src/compare/actions.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { FETCH_SPECIFIC_PLAYERS } from './actionTypes';
 
-export const fetchSpecificPlayers = (playerId) => {
+const fetchSpecificPlayers = (playerId) => {
   const request = axios.post('/api/getPlayersByName', playerId);
   return {
     type: 'FETCH_SPECIFIC_PLAYERS',
     payload: request,
   };
 };
+
+export default fetchSpecificPlayers;
