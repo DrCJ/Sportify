d29d6471db8069a09009e7765ada2f93b09e9862
Dennis Ting <dennisting@gmail.com>
2016-08-22 21:49:40 -0700

    Add styling to the schedule page

--- client/src/schedule/components/scheduleGamesListEntry.jsx
+++ client/src/schedule/components/scheduleGamesListEntry.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
 import teamImages from '../teamsImageConstant';
 
-const ScheduleGamesListEntry = ({ awayTeam, homeTeam, date }) => (
-  <div className="schedule-game">
-    <span className="team1">{awayTeam + date}
-      <img className="schedule-image" alt={awayTeam} src={teamImages[awayTeam]} />
-    </span>
-    <span className="team2">{homeTeam}
-      <img className="schedule-image" alt={homeTeam} src={teamImages[homeTeam]} />
-    </span>
-  </div>
-);
+class ScheduleGamesListEntry extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const date = this.props.date;
+    const awayTeam = this.props.awayTeam;
+    const homeTeam = this.props.homeTeam;
+    const momentDate = moment(date);
+    const monthNum = momentDate.month();
+    const convertedDate = momentDate._d;
+
+    const monthDayNum = momentDate.date();
+    const yearNum = momentDate.year();
+
+    const team1 = `team1 team1-${awayTeam}`;
+    const team2 = `team2 team2-${homeTeam}`;
+
+    return (
+
+      <div className="schedule-game">
+        <span className={team1}>{awayTeam}
+          <img className="schedule-image" alt={awayTeam} src={teamImages[awayTeam]} />
+        </span>
+        <span className="schedule-time"> {convertedDate.toString()} </span>
+        <span className={team2} >{homeTeam}
+          <img className="schedule-image" alt={homeTeam} src={teamImages[homeTeam]} />
+        </span>
+      </div>
+    )
+  }
+};
 
 export default ScheduleGamesListEntry;
