ed623775983abea1238df01198aee8daa0d2af08
Carlos Espinal <cespinalt@gmail.com>
2016-08-26 16:00:09 -0700

    Add styles for footer

--- client/src/footer/index.jsx
+++ client/src/footer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer id="footer">
-    <div>
+    <div className="center-content">
       <p>&copy; 2016 Sportify</p>
     </div>
   </footer>
