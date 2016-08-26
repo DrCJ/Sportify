e802e50f563d4308432da23a57c16b5f3780d0df
Carlos Espinal <cespinalt@gmail.com>
2016-08-26 10:34:22 -0700

    Commit to merge

--- client/src/header/actions.js
+++ client/src/header/actions.js
@@ -4,8 +4,10 @@ export function toggleNavigation() {
   const navCanvasClick = document.getElementsByClassName('nav-canvas');
   const mainContentClick = document.getElementsByClassName('main-content');
   const shadowClick = document.getElementsByClassName('shadow');
+  const menuBtn = document.getElementsByClassName('menu-btn');
 
-  const navOpen = () => {
+  const navOpen = (evt) => {
+    menuBtn[0].className = 'menu-btn open';
     navCanvasClick[0].className = 'nav-canvas-clicked';
     mainContentClick[0].className = 'main-content-clicked';
     shadowClick[0].className = 'shadow-click';
